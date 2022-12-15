c4a3172bb1815a2d672b581b25a2e02bed0c8b43
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-12-15 20:07:00 +0000

    Update shapes.py

--- Santa Hat Bauble/shapes.py
+++ Santa Hat Bauble/shapes.py
@@ -1,1 +1,47 @@
+import turtle 
 
+  
+
+def draw_circle(turtle, color, x, y, radius): 
+
+  turtle.penup() 
+
+  turtle.color(color) 
+
+  turtle.fillcolor(color) 
+
+  turtle.goto(x,y) 
+
+  turtle.pendown() 
+
+  turtle.begin_fill() 
+
+  turtle.circle(radius) 
+
+  turtle.end_fill() 
+
+     
+
+def draw_triangle(turtle, color, x, y, size): 
+
+  turtle.penup() 
+
+  turtle.color(color) 
+
+  turtle.fillcolor(color) 
+
+  turtle.goto(x,y) 
+
+  turtle.pendown() 
+
+  turtle.begin_fill() 
+
+  for i in range (3): 
+
+    turtle.forward(size) 
+
+    turtle.left(120) 
+
+  turtle.end_fill() 
+
+  turtle.setheading(0) 
